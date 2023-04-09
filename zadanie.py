651ac4d86b42e9deb8cf01ce1ce80a1f588e62cd
marchel99 <kubamarchelski@live.com>
2023-04-09 21:06:23 +0200

    Your branch is up to date with 'origin/master'.
    Changes to be committed:

--- zadanie.py
+++ zadanie.py
@@ -1,45 +1,33 @@
 import random
-from _csv import writer
 import numpy as np
 import math
 
-def generate_points_on_surface(num_points, width, length):
-    points = []
-    for i in range(num_points):
-        x = random.uniform(-width / 2, width / 2)  
-        y = random.uniform(-length / 2, length / 2) 
-        z = 0  
-        points.append([x, y, z])
-    return points
-
-def generate_points_on_surface2(num_points, width, length, height):
-    points = []
-    for i in range(num_points):
-        x = random.uniform(-width / 2, width / 2)  
-        y = random.uniform(-length / 2, length / 2)  
-        z = random.uniform(0, height)  
-        points.append([x, y, z])
-    return points
+def generate_points_on_surface(num_points, width, length, height=0):
+    if height == 0:
+        points = np.random.rand(num_points, 3) - 0.5
+        points[:, 0] *= width
+        points[:, 1] *= length
+        points[:, 2] = 0
+    else:
+        points = np.random.rand(num_points, 3)
+        points[:, 0] = (np.random.rand(num_points) - 0.5) * width
+        points[:, 1] = (np.random.rand(num_points) - 0.5) * length
+        points[:, 2] *= height
+    return points.tolist()
 
 def generate_points_on_cylinder(num_points, radius, height):
-    points = []
-    for i in range(num_points):
-        theta = random.uniform(0, 2 * math.pi)  
-        z = random.uniform(0, height)  
-        x = radius * math.cos(theta)  
-        y = radius * math.sin(theta)  
-        points.append([x, y, z])
-    return points
+    thetas = np.random.rand(num_points) * 2 * math.pi
+    zs = np.random.rand(num_points) * height
+    xs = radius * np.cos(thetas)
+    ys = radius * np.sin(thetas)
+    points = np.vstack([xs, ys, zs]).T
+    return points.tolist()
 
 if __name__ == '__main__':
-    points=generate_points_on_surface(10000,10,10)
-    points2=generate_points_on_surface2(10000, 10, 0, 10)
-    points3=generate_points_on_cylinder(10000, 10, 10)
+    points = generate_points_on_surface(10000, 10, 10)
+    points2 = generate_points_on_surface(10000, 10, 0, 10)
+    points3 = generate_points_on_cylinder(10000, 10, 10)
     with open('xyz.xyz', 'w', newline='') as csvfile:
         csvwriter = writer(csvfile)
-        for point in points:
-            csvwriter.writerow(point)
-        for point in points2:
-            csvwriter.writerow(point)
-        for point in points3:
+        for point in points + points2 + points3:
             csvwriter.writerow(point)
