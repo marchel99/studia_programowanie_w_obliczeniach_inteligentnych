a375298779b1a6808ee89aa3ac45ac57d99d847c
marchel99 <kubamarchelski@live.com>
2023-04-09 21:00:29 +0200

    Changes to be committed: 	new file:   points.xyz 	new file:   xyz.xyz

--- zadanie.py
+++ zadanie.py
@@ -1,56 +1,45 @@
+import random
+from _csv import writer
 import numpy as np
-import csv
+import math
 
-def fit_plane_ransac(points, num_iterations=50, threshold_distance=0.1, inlier_ratio=0.8):
-    n_points = points.shape[0]
-    best_inliers = None
-    best_model = None
-    for n in range(num_iterations):
-        # randomly select three points from the point cloud
-        sample_indices = np.random.choice(n_points, 3, replace=False)
-        sample = points[sample_indices]
-        # calculate the normal vector of the plane
-        normal = np.cross(sample[1] - sample[0], sample[2] - sample[0])
-        normal /= np.linalg.norm(normal)
-        # calculate the distance of all points from the plane
-        distances = np.abs((points - sample[0]).dot(normal))
-        # determine the inliers and outliers based on the threshold distance
-        inliers = points[distances < threshold_distance]
-        outliers = points[distances >= threshold_distance]
-        # if the number of inliers is greater than the inlier ratio, update the best model
-        if inliers.shape[0] > inlier_ratio * n_points and (best_inliers is None or inliers.shape[0] > best_inliers.shape[0]):
-            best_inliers = inliers
-            # calculate the normal vector of the plane based on all inliers
-            best_normal = np.cross(best_inliers[1] - best_inliers[0], best_inliers[2] - best_inliers[0])
-            best_normal /= np.linalg.norm(best_normal)
-            best_model = (best_normal, np.mean(best_inliers, axis=0))
-    return best_model
+def generate_points_on_surface(num_points, width, length):
+    points = []
+    for i in range(num_points):
+        x = random.uniform(-width / 2, width / 2)  
+        y = random.uniform(-length / 2, length / 2) 
+        z = 0  
+        points.append([x, y, z])
+    return points
 
-# read the xyz file and store the points as a numpy array
-points = []
-with open('xyz.txt', 'r') as file:
-    reader = csv.reader(file, delimiter=' ')
-    for row in reader:
-        points.append([float(row[0]), float(row[1]), float(row[2])])
-points = np.array(points)
+def generate_points_on_surface2(num_points, width, length, height):
+    points = []
+    for i in range(num_points):
+        x = random.uniform(-width / 2, width / 2)  
+        y = random.uniform(-length / 2, length / 2)  
+        z = random.uniform(0, height)  
+        points.append([x, y, z])
+    return points
 
-# find three disjoint point clouds using k-means clustering
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=3, random_state=0).fit(points)
-labels = kmeans.labels_
+def generate_points_on_cylinder(num_points, radius, height):
+    points = []
+    for i in range(num_points):
+        theta = random.uniform(0, 2 * math.pi)  
+        z = random.uniform(0, height)  
+        x = radius * math.cos(theta)  
+        y = radius * math.sin(theta)  
+        points.append([x, y, z])
+    return points
 
-# fit a plane to each point cloud using RANSAC and determine if it is horizontal, vertical or neither
-for i in range(3):
-    cloud = points[labels == i]
-    model = fit_plane_ransac(cloud)
-    normal = model[0]
-    mean_distance = np.abs((cloud - model[1]).dot(normal)).mean()
-    if mean_distance < 0.05:
-        if np.abs(normal[2]) > 0.9:
-            print('Cloud', i+1, 'represents a horizontal plane')
-        elif np.abs(normal[0]) > 0.9:
-            print('Cloud', i+1, 'represents a vertical plane')
-        else:
-            print('Cloud', i+1, 'does not represent a horizontal or vertical plane')
-    else:
-        print('Cloud', i+1, 'does not represent a plane')
+if __name__ == '__main__':
+    points=generate_points_on_surface(10000,10,10)
+    points2=generate_points_on_surface2(10000, 10, 0, 10)
+    points3=generate_points_on_cylinder(10000, 10, 10)
+    with open('xyz.xyz', 'w', newline='') as csvfile:
+        csvwriter = writer(csvfile)
+        for point in points:
+            csvwriter.writerow(point)
+        for point in points2:
+            csvwriter.writerow(point)
+        for point in points3:
+            csvwriter.writerow(point)
